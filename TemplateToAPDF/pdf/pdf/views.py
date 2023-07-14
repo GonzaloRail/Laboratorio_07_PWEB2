87af528bb5236dfd9fb05e7021804836f5756904
GonzaloRail <glaymem@unsa.edu.pe>
2023-07-14 03:10:16 -0500

    se creo el views.py

---
+++ TemplateToAPDF/pdf/pdf/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template.loader import get_template
+from pdf.utils import render_to_pdf #created in step 4
+
+class GeneratePdf(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('pdf/invoice.html')
+        context ={
+            "invoice_id" : 123,
+            "customer_name" : "Gonzalo",
+            "amount" : 1399.99,
+            "today" : "hoy",
+        }
+        html = template.render(context)
+        pdf = render_to_pdf('pdf/invoice.html',context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf" %("123411231")
+            content = "inLine; filename='%s'" %(filename)
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not found")
