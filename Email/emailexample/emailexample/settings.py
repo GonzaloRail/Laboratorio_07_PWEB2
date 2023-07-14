fdbb332ad397597f45f3d0b8484509174b42ff97
GonzaloRail <glaymem@unsa.edu.pe>
2023-07-14 04:32:11 -0500

    Se creo urls y se modifico settings

--- Email/emailexample/emailexample/settings.py
+++ Email/emailexample/emailexample/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'send'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+#EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_USE_SSL=False
+EMAIL_PORT=587
+EMAIL_HOST_USER="azel987654321@gmail.com"
+EMAIL_HOST_PASSWORD="znineedmgstmarmw"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
