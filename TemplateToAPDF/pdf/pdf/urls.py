e704ef849310d7026cb9a3723c247445114e918f
GonzaloRail <glaymem@unsa.edu.pe>
2023-07-14 03:10:28 -0500

    Se añadio la url

--- TemplateToAPDF/pdf/pdf/urls.py
+++ TemplateToAPDF/pdf/pdf/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pdf.views import GeneratePdf
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls) ,
+    path('pdf/',GeneratePdf.as_view()),
 ]
